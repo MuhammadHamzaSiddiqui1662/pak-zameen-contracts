036cb6c9ad4e2046fc2d679cbe3fa0d7d40d6c87
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-04-24 18:04:29 +0500

    feat: test foe registrar upgraded

--- test/Registrar.ts
+++ test/Registrar.ts
@@ -15,28 +15,44 @@ describe("Registrar", function () {
         // Constants required
         const societyName = "Gulberg";
         const societySymbol = "GLB";
-        const potentialOwners = [otherAccount[0].address, otherAccount[1].address, otherAccount[2].address];
+        const potentialOwners = [
+            owner.address,
+            otherAccount[0].address,
+            otherAccount[1].address,
+            otherAccount[2].address,
+        ];
         const plotAddresses = [
             ethers.utils.formatBytes32String("A-930 Block-12 Gulberg"),
             ethers.utils.formatBytes32String("A-101 Block-12 Gulberg"),
             ethers.utils.formatBytes32String("B-506 Block-12 Gulberg"),
+            ethers.utils.formatBytes32String("C-26 Block-12 Gulberg"),
         ];
 
         const Registrar = await ethers.getContractFactory("Registrar");
         const registrar = await Registrar.deploy();
 
-        return { registrar, societyName, societySymbol, potentialOwners, plotAddresses, owner, otherAccount };
+        const societyRecipt = await (
+            await registrar.createSociety(societyName, societySymbol, plotAddresses, potentialOwners)
+        ).wait();
+
+        const societyAddress = await registrar.societyToAddress(societyName);
+
+        return {
+            registrar,
+            societyName,
+            societySymbol,
+            potentialOwners,
+            plotAddresses,
+            owner,
+            otherAccount,
+            societyAddress,
+            societyRecipt,
+        };
     }
 
     describe("Deployment", function () {
         it("Should create a valid scoiety", async function () {
-            const { registrar, societyName, societySymbol, plotAddresses, potentialOwners } = await loadFixture(
-                DeployRegistrar
-            );
-            console.log(societyName);
-            const societyRecipt = await (
-                await registrar.createSociety(societyName, societySymbol, plotAddresses, potentialOwners)
-            ).wait();
+            const { registrar, societyName, societyRecipt } = await loadFixture(DeployRegistrar);
             const societyAddressByEvent = (await parseEventLogs(societyRecipt.transactionHash, abi))[0].args
                 .societyAddress;
             const societyAddressByContractMapping = await registrar.societyToAddress(societyName);
