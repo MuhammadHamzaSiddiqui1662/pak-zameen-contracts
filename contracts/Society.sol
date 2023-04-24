fc7fa24fddc3c205b468061d2821bad814b87db0
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-04-24 18:03:16 +0500

    feat: Society contract upgraded

--- contracts/Society.sol
+++ contracts/Society.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "hardhat/console.sol";
 
 contract Society is ERC721 {
     uint256 public immutable i_deplotTimestamp;
@@ -12,19 +13,8 @@ contract Society is ERC721 {
     mapping(bytes32 => address) plotAddressToPotentialOwner;
     // token counter
     uint256 private s_tokenCounter;
-    mapping(uint256 => bytes32) tokenCounterToTicker;
+    mapping(bytes32 => uint256) plotAddressToTokenId;
     mapping(bytes32 => address) plotAddToOwner;
-    // sell id
-    uint256 s_sellId;
-    struct Sale {
-        address owner;
-        bytes32 plotAdd;
-        uint256 price;
-        bool isOpen;
-    }
-    mapping(uint256 => Sale) saleIdToSaleOffer;
-
-    event SaleOffer(address indexed owner, bytes32 indexed plotAdd, uint256 indexed price, bool isOpen);
 
     constructor(
         string memory name,
@@ -33,10 +23,10 @@ contract Society is ERC721 {
         address[] memory potentialOwners
     ) ERC721(name, symbol) {
         require(plotAddresses.length == potentialOwners.length, "Incorrect data provided");
+
         i_deplotTimestamp = block.timestamp;
         i_registrar = msg.sender;
         s_tokenCounter = 0;
-        s_sellId = 0;
         s_plotAddresses = plotAddresses;
         s_potentialOwners = potentialOwners;
         for (uint i = 0; i < plotAddresses.length; i++) {
@@ -44,11 +34,14 @@ contract Society is ERC721 {
         }
     }
 
-    function mintNFT(bytes32 plotAdd) public onlyPotentialOwner(plotAdd) returns (uint256) {
-        _safeMint(msg.sender, s_tokenCounter);
+    function mintNFT(
+        address caller,
+        bytes32 plotAdd
+    ) public onlyRegistrar onlyPotentialPlotOwner(caller, plotAdd) returns (uint256) {
+        _safeMint(caller, s_tokenCounter);
         approve(i_registrar, s_tokenCounter);
-        tokenCounterToTicker[s_tokenCounter] = plotAdd;
-        plotAddToOwner[plotAdd] = msg.sender;
+        plotAddressToTokenId[plotAdd] = s_tokenCounter;
+        plotAddToOwner[plotAdd] = caller;
         s_tokenCounter = s_tokenCounter + 1;
         return s_tokenCounter;
     }
@@ -57,14 +50,27 @@ contract Society is ERC721 {
         return s_tokenCounter;
     }
 
-    function generateSellOffer(bytes32 plotAdd, uint256 price) public returns (uint256) {
-        require(plotAddToOwner[plotAdd] == msg.sender, "Not Owner of Plot.");
-        saleIdToSaleOffer[s_sellId] = Sale(msg.sender, plotAdd, price, true);
-        s_sellId = s_sellId + 1;
-        return s_sellId - 1;
+    function isPlotOwner(address caller, bytes32 plotAdd) public view onlyPlotOwner(caller, plotAdd) {}
+
+    function getTokenIdOfPlot(bytes32 plotAdd) public view returns (uint256) {
+        return plotAddressToTokenId[plotAdd];
     }
 
-    modifier onlyPotentialOwner(bytes32 plotAdd) {
+    function closeSaleOffer(address owner, address buyer, uint256 tokenId) public onlyRegistrar {
+        safeTransferFrom(owner, buyer, tokenId);
+    }
+
+    modifier onlyRegistrar() {
+        require(msg.sender == i_registrar);
+        _;
+    }
+
+    modifier onlyPlotOwner(address caller, bytes32 plotAdd) {
+        require(plotAddToOwner[plotAdd] == msg.sender, "Not Owner of Plot.");
+        _;
+    }
+
+    modifier onlyPotentialPlotOwner(address caller, bytes32 plotAdd) {
         require(
             msg.sender == plotAddressToPotentialOwner[plotAdd] ||
                 block.timestamp > (i_deplotTimestamp + 6 * 30.44 days),
