5b9b4330eae00333d619a97136daa5873f1a5727
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-04-24 19:59:08 +0500

    feat: deploy script added

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,21 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+    const accounts = await ethers.getSigners();
+    const deployer = accounts[0];
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+    const Registrar = await ethers.getContractFactory("Registrar");
+    const registrar = await Registrar.deploy();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    await registrar.deployed();
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+    console.log(`Registrar Contract Deployed at address: ${registrar.address} by signer: ${deployer.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
+
